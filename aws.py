317b285efe713e9b6014065334e715940c49244a
aki. <mark@aur.ll.to>
2025-05-28 08:45:48 +0900

    .

--- aws.py
+++ aws.py
@@ -15,10 +15,12 @@ https://platform.openai.com/docs/api-reference/chat/create
 """
 
 import asyncio
+import base64
 import collections.abc
 import json
 import logging
 import os
+import pathlib
 import time
 import typing
 import uuid
@@ -27,6 +29,7 @@ import aiobotocore.session
 import iam_rolesanywhere_session
 import openai.types.chat
 import openai.types.completion_usage
+import pytilpack.data_url
 import types_aiobotocore_bedrock_runtime.type_defs
 from openai._types import NOT_GIVEN
 
@@ -145,63 +148,323 @@ class AWSClient:
     def _format_messages(
         self, messages: typing.Iterable[openai.types.chat.ChatCompletionMessageParam]
     ) -> tuple[
-        list[types_aiobotocore_bedrock_runtime.type_defs.MessageTypeDef],
+        list[types_aiobotocore_bedrock_runtime.type_defs.MessageUnionTypeDef],
         list[types_aiobotocore_bedrock_runtime.type_defs.SystemContentBlockTypeDef],
     ]:
         """メッセージをBedrockの形式に変換します。"""
         formatted_messages: list[
-            types_aiobotocore_bedrock_runtime.type_defs.MessageTypeDef
+            types_aiobotocore_bedrock_runtime.type_defs.MessageUnionTypeDef
         ] = []
         for message in messages:
-            role: typing.Literal["assistant", "user"]
-            content: list[
-                types_aiobotocore_bedrock_runtime.type_defs.ContentBlockTypeDef
-            ]
-            # TODO: role
-            # 'developer', 'system', 'user', 'assistant', 'tool', 'function'
             if message["role"] == "tool":
-                role = "user"
+                tool_result = self._to_bedrock_tool_result(message)
 
-                content_data = message["content"]
-                try:
-                    content_data = {"json": json.loads(content_data)}
-                except json.JSONDecodeError:
-                    content_data = {"text": content_data}
-
-                content = [
-                    {
-                        "toolResult": {
-                            "toolUseId": message["tool_call_id"],
-                            "content": [content_data],
-                            # "status": "error"  ⇒ OpenAI APIにはこれに相当するものが無い…
-                        }
-                    }
-                ]
+                # 複数あったらまとめる
+                if (
+                    len(formatted_messages) > 0
+                    and formatted_messages[-1].get("role") == "user"
+                ):
+                    # 最後のメッセージに追加
+                    assert isinstance(formatted_messages[-1]["content"], list)
+                    formatted_messages[-1]["content"].append(tool_result)
+                else:
+                    # 新しいメッセージとして追加
+                    formatted_messages.append(
+                        {"role": "user", "content": [tool_result]}
+                    )
 
             elif message["role"] in ("user", "assistant"):
-                role: typing.Literal["assistant", "user"] = message["role"]
-                if isinstance(message["content"], list):
-                    # TODO: image_urlなど
-                    content = message["content"]
-                elif isinstance(message["content"], str):
-                    content = [{"text": message["content"]}]
-                else:
-                    raise ValueError(
-                        f"Invalid content type: {type(message['content'])}. "
-                        "Expected str or list."
-                    )
+                formatted_messages.append(
+                    self._to_bedrock_userassistant_message(message)
+                )
             else:
                 continue  # 'developer', 'system', 'function' は無視
 
-            formatted_messages.append({"role": role, "content": content})
-
-        system = [
-            {"text": message["content"]}
-            for message in messages
-            if message["role"] in ("developer", "system")
-        ]
+        system = sum(
+            [
+                typing.cast(
+                    list[
+                        types_aiobotocore_bedrock_runtime.type_defs.SystemContentBlockTypeDef
+                    ],
+                    self._to_bedrock_content_blocks(message["content"]),
+                )
+                for message in messages
+                if message["role"] in ("developer", "system")
+            ],
+            [],
+        )
 
         return formatted_messages, system
+
+    def _to_bedrock_userassistant_message(
+        self,
+        message: (
+            openai.types.chat.ChatCompletionUserMessageParam
+            | openai.types.chat.ChatCompletionAssistantMessageParam
+        ),
+    ) -> types_aiobotocore_bedrock_runtime.type_defs.MessageUnionTypeDef:
+        role = typing.cast(typing.Literal["assistant", "user"], message["role"])
+        content = self._to_bedrock_content_blocks(message["content"])
+        # audio
+        audio = message.get("audio")
+        if audio is not None:
+            logger.warning(
+                f"Audio content is not supported in this implementation. {audio=}"
+            )
+            # tool_calls
+        tool_calls = message.get("tool_calls", [])
+        for tool_call in tool_calls:
+            if tool_call.get("type") != "function":
+                raise ValueError(
+                    f"Unsupported tool call type: {tool_call=}. " "Expected 'function'."
+                )
+            function = tool_call.get("function")
+            if function is None:
+                raise ValueError(f"Tool call must have 'function'. {tool_call=}")
+            content.append(
+                {
+                    "toolUse": {
+                        "toolUseId": tool_call["id"],
+                        "name": function.get("name", ""),
+                        "input": function.get("arguments", ""),
+                    }
+                }
+            )
+            # refusal
+        refusal = message.get("refusal")
+        if refusal is not None:
+            content.append({"guardContent": {"text": refusal}})
+        return {"role": role, "content": content}
+
+    def _to_bedrock_tool_result(
+        self, message: openai.types.chat.ChatCompletionToolMessageParam
+    ) -> types_aiobotocore_bedrock_runtime.type_defs.ContentBlockTypeDef:
+        tool_call_id = message.get("tool_call_id")
+        if tool_call_id is None:
+            raise ValueError(f"Tool message must have 'tool_call_id'. {message=}")
+        content = typing.cast(
+            list[
+                types_aiobotocore_bedrock_runtime.type_defs.ToolResultContentBlockTypeDef
+            ],
+            self._to_bedrock_content_blocks(message["content"], unjsonify=True),
+        )
+        return {
+            "toolResult": {
+                "toolUseId": tool_call_id,
+                "content": content,
+                # "status": "error"  ⇒ OpenAI APIにはこれに相当するものが無い…
+            }
+        }
+
+    def _to_bedrock_content_blocks(
+        self,
+        value: (
+            str
+            | typing.Iterable[
+                openai.types.chat.chat_completion_content_part_param.ChatCompletionContentPartParam
+            ]
+            | typing.Iterable[
+                openai.types.chat.chat_completion_assistant_message_param.ContentArrayOfContentPart
+            ]
+            | typing.Iterable[
+                openai.types.chat.chat_completion_tool_message_param.ChatCompletionContentPartTextParam
+            ]
+            | None
+        ),
+        unjsonify: bool = False,
+    ) -> list[types_aiobotocore_bedrock_runtime.type_defs.ContentBlockTypeDef]:
+        if value is None:
+            return []
+        if isinstance(value, str):
+            return [{"text": value}]
+        if isinstance(value, collections.abc.Iterable):
+            return [self._to_bedrock_content_block(v, unjsonify) for v in value]
+        raise ValueError(
+            f"Invalid content: {value=}. "
+            "Expected str or iterable of OpenAI content parts."
+        )
+
+    def _to_bedrock_content_block(
+        self,
+        value: (
+            openai.types.chat.chat_completion_content_part_param.ChatCompletionContentPartParam
+            | openai.types.chat.chat_completion_assistant_message_param.ContentArrayOfContentPart
+            | openai.types.chat.chat_completion_tool_message_param.ChatCompletionContentPartTextParam
+        ),
+        unjsonify: bool = False,
+    ) -> types_aiobotocore_bedrock_runtime.type_defs.ContentBlockTypeDef:
+        """OpenAIのコンテンツをBedrockの形式に変換します。"""
+
+        # class ContentBlockTypeDef(TypedDict):
+        #     text: NotRequired[str]
+        #     image: NotRequired[ImageBlockUnionTypeDef]
+        #     document: NotRequired[DocumentBlockUnionTypeDef]
+        #     video: NotRequired[VideoBlockUnionTypeDef]
+        #     toolUse: NotRequired[ToolUseBlockUnionTypeDef]
+        #     toolResult: NotRequired[ToolResultBlockUnionTypeDef]
+        #     guardContent: NotRequired[GuardrailConverseContentBlockUnionTypeDef]
+        #     reasoningContent: NotRequired[ReasoningContentBlockUnionTypeDef]
+
+        # 返り値の型は unjsonify=Trueなら ToolResultContentBlockOutputTypeDef だけど手抜き
+
+        match value.get("type"):
+            case "text":
+                text = value.get("text")
+                if text is None:
+                    raise ValueError(f"Text content must have 'text' field. {value=}")
+                text = str(text)
+                if unjsonify:
+                    # 文字列がJSONっぽければJSON扱いにしちゃう
+                    try:
+                        data = json.loads(text)
+                        # 偶然の一致を避けるため一応dict/listのみにする
+                        if isinstance(data, (dict, list)):
+                            return {"json": data}  # type: ignore[return-value]
+                    except json.JSONDecodeError:
+                        pass
+                return {"text": value["text"]}
+            case "image_url":
+                image_url = value.get("image_url")
+                if image_url is None:
+                    raise ValueError(
+                        f"Image URL content must have 'image_url' field. {value=}"
+                    )
+                url = image_url.get("url")
+                if url is None:
+                    raise ValueError(
+                        f"Image URL content must have 'url' field. {image_url=}"
+                    )
+                # detail = image_url.get("detail")
+                data_url = pytilpack.data_url.parse(url)
+                if data_url.mime_type.startswith("image/"):
+                    # ["gif", "jpeg", "png", "webp"]
+                    image_format = {
+                        "image/gif": "gif",
+                        "image/jpeg": "jpeg",
+                        "image/webp": "webp",
+                        "image/png": "png",
+                    }.get(data_url.mime_type)
+                    if image_format is None:
+                        raise ValueError(
+                            f"Unsupported MIME type: {data_url.mime_type}. "
+                            "Expected image."
+                        )
+                    return {
+                        "image": {
+                            "format": image_format,
+                            "source": {"bytes": data_url.data},
+                        }
+                    }
+                elif data_url.mime_type.startswith("video/"):
+                    # ["flv", "mkv", "mov", "mp4", "mpeg", "mpg", "three_gp", "webm", "wmv"]
+                    video_format = {
+                        "video/x-flv": "flv",
+                        "video/x-matroska": "mkv",
+                        "video/quicktime": "mov",
+                        "video/mp4": "mp4",
+                        "video/mpeg": "mpeg",
+                        "video/3gpp": "three_gp",
+                        "video/webm": "webm",
+                        "video/x-ms-wmv": "wmv",
+                        "video/x-msvideo": "avi",
+                        "video/ogg": "ogg",
+                    }.get(data_url.mime_type)
+                    if video_format is None:
+                        raise ValueError(
+                            f"Unsupported MIME type: {data_url.mime_type}. "
+                            "Expected video."
+                        )
+                    return {
+                        "video": {
+                            "format": video_format,
+                            "source": {"bytes": data_url.data},
+                        }
+                    }
+                else:
+                    # ["pdf", "txt", "csv", "html", "doc", "docx", "xls", "xlsx"]
+                    doc_format = {
+                        "application/pdf": "pdf",
+                        "text/plain": "txt",
+                        "text/csv": "csv",
+                        "text/html": "html",
+                        "application/msword": "doc",
+                        "application/vnd.openxmlformats-officedocument.wordprocessingml.document": "docx",
+                        "application/vnd.ms-excel": "xls",
+                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": "xlsx",
+                    }.get(data_url.mime_type)
+                    if doc_format is None:
+                        raise ValueError(
+                            f"Unsupported MIME type: {data_url.mime_type}. "
+                            "Expected image/video/document."
+                        )
+                    return {
+                        "document": {
+                            "format": doc_format,
+                            "source": {"bytes": data_url.data},
+                        }
+                    }
+            case "input_audio":
+                # format_ = value["input_audio"].get("format")  # "wav" or "mp3"
+                # data = value["input_audio"].get("data", "")  # Base64 encoded data.
+                raise ValueError("Input audio is not supported in this implementation.")
+            case "file":
+                file = value.get("file", {})
+                file_data = file.get("file_data", "")  # Base64 encoded data.
+                # file_id = file.get("file_id")  # uploaded file ID.
+                filename = file.get("filename")  # Original filename.
+                if file_data is None:
+                    # 手抜き: とりあえずファイルデータ必須にする
+                    raise ValueError(
+                        "File data is required for file content in this implementation."
+                    )
+                suffix = pathlib.Path(filename).suffix if filename else ""
+                # ["gif", "jpeg", "jpg", "png", "webp"]
+                # ["flv", "mkv", "mov", "mp4", "mpeg", "mpg", "three_gp", "webm", "wmv"]
+                # ["pdf", "txt", "csv", "html", "doc", "docx", "xls", "xlsx"]
+                type_ = {
+                    ".gif": "image",
+                    ".jpeg": "image",
+                    ".jpg": "image",
+                    ".png": "image",
+                    ".webp": "image",
+                    ".flv": "video",
+                    ".mkv": "video",
+                    ".mov": "video",
+                    ".mp4": "video",
+                    ".avi": "video",
+                    ".mpeg": "video",
+                    ".mpg": "video",
+                    ".3gp": "video",
+                    ".webm": "video",
+                    ".wmv": "video",
+                    ".pdf": "document",
+                    ".txt": "document",
+                    ".csv": "document",
+                    ".html": "document",
+                    ".doc": "document",
+                    ".docx": "document",
+                    ".xls": "document",
+                    ".xlsx": "document",
+                }.get(suffix.lower())
+                format_ = suffix.removeprefix(".")
+                format_ = {
+                    # ほぼ拡張子のままいけるがダメなやつだけ置換
+                    "3gp": "three_gp"
+                }.get(format_, format_)
+                return {
+                    type_: {
+                        "format": format_,
+                        "source": {"bytes": base64.b64decode(file_data)},
+                    }
+                }
+            case "refusal":
+                refusal = value.get("refusal")
+                return {"guardContent": {"text": refusal}}
+            case _:
+                raise ValueError(
+                    f"Unsupported content type: {value.get('type')}. "
+                    "Expected 'text', 'image_url', 'file_url', or 'refusal'."
+                )
 
     def _make_inference_config(
         self, request: types_chat.ChatRequest
