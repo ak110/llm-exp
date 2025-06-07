797207a6d04544ffe507212c59b38764d39a7670
aki. <mark@aur.ll.to>
2025-06-07 11:39:07 +0900

    .

--- litellm_exp.py
+++ litellm_exp.py
@@ -10,7 +10,11 @@ def _main():
     # Amazon Nova Pro: $0.0008, $0.0032
     model = "amazon.nova-pro-v1:0"
 
-    messages = [{"user": "role", "content": "Hey, how"}]
+    messages = [
+        {"role": "user", "content": "Hey, how are you?"},
+        {"role": "assistant", "content": "Hey, how are you doing today?"},
+        {"role": "user", "content": "Waaaah, I'm so happy to see you!"},
+    ]
     output_message = "Hey, how are you doing today?"
     input_tokens = litellm.token_counter(
         model=model,
@@ -23,19 +27,19 @@ def _main():
         count_response_tokens=False,  # Trueだと何かがずれる
     )
     print(f"InputTokens: {input_tokens}")
-    print(f"OutputTokens: {input_tokens}")
+    print(f"OutputTokens: {output_tokens}")
 
     input_cost = litellm.completion_cost(model=model, messages=messages)
-    print(f"InputCost/1mTokens: {input_cost * 1e6 / input_tokens}")
+    print(f"InputCost/1mTokens: {input_cost * 1e6 / input_tokens:.3f}")
 
     output_cost = litellm.completion_cost(model=model, completion=output_message)
-    print(f"OutputCost/1mTokens: {output_cost * 1e6 / output_tokens}")
+    print(f"OutputCost/1mTokens: {output_cost * 1e6 / output_tokens:.3f}")
 
     total_cost = litellm.completion_cost(
         model=model, messages=messages, completion=output_message
     )
-    print(f"InputCost+OutputCost: {input_cost + output_cost}")
-    print(f"TotalCost: {total_cost}")
+    print(f"InputCost+OutputCost: {input_cost + output_cost:.9f}")
+    print(f"TotalCost: {total_cost:.9f}")
 
 
 if __name__ == "__main__":
